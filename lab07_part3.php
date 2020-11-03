cc529825983109084e1c1f6480239378bf800b55
agnesjee <68498830+agnesjee@users.noreply.github.com>
2020-11-03 17:58:48 +0800

    Create lab07_part3.php

---
+++ lab07_part3.php
@@ -0,0 +1,17 @@
+Select your favorite color:
+<input type="color" name="favcolor" value="#0000ff"><br/>
+Birthday:
+<input type="date" name="bday" value="2013-09-02"><br/>
+E-mail:
+<input type="email" name="email"><br/>
+Quantity (between 1 and 5):
+<input type="number" name="quantity"
+   min="1" max="5"><br/>
+Add your homepage:
+<input type="url" name="homepage"><br/>
+Transportation:
+<input type="flying" name="saucer"><br/>
+Enter your phone number:
+<input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" ><br/>
+Select a time:
+<input type="time" name="time"><br/>
